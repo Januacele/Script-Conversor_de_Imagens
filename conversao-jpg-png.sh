0ec31a88b6525767a0291df59e91fa4262cec062
Januacele <januacelevieira@hotmail.com>
2023-04-24 22:18:48 -0300

    Convertendo arquivos de jpg para png e salvando numa pasta exclusiva

--- conversao-jpg-png.sh
+++ conversao-jpg-png.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-CAMINHO_IMAGENS=~/Documentos/DevOps_Alura/Shell/imagens-livros
+cd ~/Documentos/DevOps_Alura/Shell/imagens-livros
 
-for imagem in $@
+if [ ! -d png ]
+then
+	mkdir png
+fi
+
+for imagem in *.jpg
 do
-	convert $CAMINHO_IMAGENS/$imagem.jpg $CAMINHO_IMAGENS/$imagem.png
+	imagem_sem_extensao=$(ls $imagem | awk -F. '{print $1}')
+	convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
 done

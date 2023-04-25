cdfe478792ce059aa5ae7ae059dbd9260d9f1d59
Januacele <januacelevieira@hotmail.com>
2023-04-24 22:50:33 -0300

    Exibindo mensagem de erro e salvando log de erro em arquivo txt a parte

--- conversao-jpg-png.sh
+++ conversao-jpg-png.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+converte_imagem(){
 cd ~/Documentos/DevOps_Alura/Shell/imagens-livros
 
 if [ ! -d png ]
@@ -9,6 +10,16 @@ fi
 
 for imagem in *.jpg
 do
-	imagem_sem_extensao=$(ls $imagem | awk -F. '{print $1}')
+	local imagem_sem_extensao=$(ls $imagem | awk -F. '{print $1}')
 	convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
 done
+}
+
+converte_imagem 2>erros_conversao.txt
+
+if [ $? -eq 0 ]
+then 
+	echo "Conversao realizada com sucesso"
+else 
+	echo "Houve uma falha no processo"
+fi
